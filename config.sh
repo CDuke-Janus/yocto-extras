f4082b1f2732c6196d411226e59c40d93c305595
CDuke-Janus <52255664+CDuke-Janus@users.noreply.github.com>
2019-07-12 10:05:59 -0400

    removed git pull

--- config.sh
+++ config.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "#----------------------#"
-echo "Navigating to ~/var-fslc-yocto/build_fb/tmp/work-shared/var-som-mx6/kernel-source"
-echo "#----------------------#"
-cd ~/var-fslc-yocto/build_fb/tmp/work-shared/var-som-mx6/kernel-source
-echo "Updating kernel source"
-echo "#----------------------#"
-git pull
 echo "Building config menu"
-echo "#----------------------#"
 cd ~/var-fslc-yocto
 MACHINE=var-som-mx6 DISTRO=fslc-framebuffer . setup-environment build_fb
 bitbake -c menuconfig virtual/kernel
