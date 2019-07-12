9637f5a0a230e2a10ba5b305522f13dc7cd38a12
CDuke-Janus <52255664+CDuke-Janus@users.noreply.github.com>
2019-07-12 10:04:00 -0400

    removed git pull

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-echo "#----------------------#"
-echo "Navigating to ~/var-fslc-yocto/build_fb/tmp/work-shared/var-som-mx6/kernel-source"
-echo "#----------------------#"
-cd ~/var-fslc-yocto/build_fb/tmp/work-shared/var-som-mx6/kernel-source
-echo "Updating kernel source"
-echo "#----------------------#"
-git pull
 echo "Building image"
-echo "#----------------------#"
 cd ~/var-fslc-yocto
 MACHINE=var-som-mx6 DISTRO=fslc-framebuffer . setup-environment build_fb
 bitbake fsl-image-qt5
