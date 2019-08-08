b1597d95ff27e6d571e0e5c2a04bf1b1caea4dc4
CDuke-Janus <52255664+CDuke-Janus@users.noreply.github.com>
2019-08-08 11:54:35 -0400

    Update copy-burn.sh

--- copy-burn.sh
+++ copy-burn.sh
@@ -2,7 +2,7 @@
 echo "Removing previous image"
 rm -rf ~/fsl-image-qt5-var-som-mx6.wic.gz 
 echo "Grabbing image"
-sshpass -p "password_here" scp -r username@host:~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/fsl-image-qt5-var-som-mx6.wic.gz ~/fsl-image-qt5-var-som-mx
-6.wic.gz
+sshpass -p "password_here" scp -r username@host:~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/fsl-image-qt5-var-som-mx6.wic.gz ~/fsl-image-qt5-var-som-mx6.wic.gz
 echo "Burning image"
+# Make sure to format the sd card using gparted before running
 zcat ~/fsl-image-qt5-var-som-mx6.wic.gz | sudo dd of=/dev/sdc bs=1M && sync
