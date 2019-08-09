6c49438234455e9111eb98cc1014e22e9b99a712
CDuke-Janus <52255664+CDuke-Janus@users.noreply.github.com>
2019-08-09 11:06:57 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ echo "Building image"
 cd ~/var-fslc-yocto
 MACHINE=var-som-mx6 DISTRO=fslc-framebuffer . setup-environment build_fb
 bitbake fsl-image-qt5
+zcat ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/fsl-image-qt5-var-som-mx6.wic.gz > ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/fsl-image-qt5-var-som-mx6.wic
